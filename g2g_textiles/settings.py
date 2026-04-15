e105b97ea35b6f33bf16958c6c876af5107a55a9
g2gtextiles <production@gtwog.ch>
2026-04-15 15:59:33 +0200

    Switch email to port 465 SSL

--- g2g_textiles/settings.py
+++ g2g_textiles/settings.py
@@ -118,8 +118,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --- Email ---
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.hostpoint.ch'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'G2G Textiles <production@gtwog.ch>'
